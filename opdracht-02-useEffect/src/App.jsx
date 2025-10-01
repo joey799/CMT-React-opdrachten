4ffb2eff04d5ae9755b4fe02078f3a0de29753dc
joey799 <9025423@student.zadkine.nl>
2025-10-01 11:40:06 +0200

    Opdracht 2 - UseEffect

--- opdracht-02-useEffect/src/App.jsx
+++ opdracht-02-useEffect/src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
-
+import Clock from './components/Clock'
 function App() {
-
   return (
+  
     <>
+      
       <h1>Opdracht 2 - UseEffect</h1>
+      <Clock/>
     </>
   )
 }
-
-export default App
+export default App;
