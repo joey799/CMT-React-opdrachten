66ea091588e7090ad6d9b6a0037283a5660d39e4
joey799 <9025423@student.zadkine.nl>
2025-10-06 14:02:47 +0200

    Opdracht 4 - Menu

--- opdracht-04-menu/src/App.jsx
+++ opdracht-04-menu/src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
+import Header from './components/Header'
+import MenuList from './components/MenuList'
+
+
+
 
 function App() {
 
   return (
     <>
       <h1>Opdracht 4 - Menu</h1>
+      <Header />
+      <MenuList/>
     </>
   )
 }
